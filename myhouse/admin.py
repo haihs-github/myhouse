4b4280414ef822f4afd37a082baad16996c46533
haihs-github <haihs.ptit@gmail.com>
2024-09-01 20:43:24 +0700

    done model

--- myhouse/admin.py
+++ myhouse/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Congno, Phong
+# Register your models here.
+
+
 
 # Register your models here.
+
+class CongnoAdmin(admin.ModelAdmin):
+	list_display = ("tiennha", "tiennuoc", "tiendien", "trangthai")
+
+class PhongAdmin(admin.ModelAdmin):
+	list_display = ("sodienthangtruoc", "sodienthangnay", "sonuocthangtruoc", "sonuocthangnay","tiennha")
+
+admin.site.register(Phong, PhongAdmin)
+admin.site.register(Congno, CongnoAdmin)
--- myhouse/models.py
+++ myhouse/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Congno(models.Model):
+	tiennha = models.BigIntegerField(default=0)
+	tiennuoc = models.BigIntegerField(default=0)
+	tiendien = models.BigIntegerField(default=0)
+	trangthai = models.BooleanField(default=False)
+
+class Phong(models.Model):
+	sodienthangtruoc = models.BigIntegerField(default=0)
+	sonuocthangtruoc = models.BigIntegerField(default=0)
+	sodienthangnay = models.BigIntegerField(default=0)
+	sonuocthangnay = models.BigIntegerField(default=0)
+	tiennha = models.BigIntegerField(default=0)
+	congno = models.OneToOneField(Congno, on_delete=models.PROTECT, related_name='Congno')
+
